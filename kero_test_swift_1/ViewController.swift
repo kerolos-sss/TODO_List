3fc3d3cd2fb79c1d7d9bbc007bf7e5aff40c775e
kerolos <kerolos.bisheer@innuva.com>
2017-02-19 16:21:49 +0200

    Initial Commit

---
+++ kero_test_swift_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kero_test_swift_1
+//
+//  Created by Kero on 2/19/17.
+//  Copyright © 2017 Kero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
